fb58185792a593a04542df8acee5d4a2ab4d6d98
PauSolT <68166681+PauSolT@users.noreply.github.com>
2026-02-13 18:42:27 +0100

    Update player

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public List<Element> elements;
+    int currentElement = 0;
+    void Start()
+    {
+        foreach (Element element in elements)
+        {
+            element.Init();
+        }
+        elements[currentElement].OnElementEquip?.Invoke(gameObject);
+        elements[currentElement].Actives[0].AbilityUse();
+    }
+
+    void Update()
+    {
+        
+    }   
+
+    void PreviousElement()
+    {
+        elements[currentElement].OnElementUnequip?.Invoke();
+        currentElement--;
+        if (currentElement < 0)
+        {
+            currentElement = elements.Count - 1;
+        }
+        elements[currentElement].OnElementEquip?.Invoke(gameObject);
+
+    }
+
+    void NextElement()
+    {
+        elements[currentElement].OnElementUnequip?.Invoke();
+        currentElement++;
+        if (currentElement >= elements.Count)
+        {
+            currentElement = 0;
+        }
+        elements[currentElement].OnElementEquip?.Invoke(gameObject);
+    }
+
+    public T GetSpecificElement<T>() where T : Element
+    {
+        foreach (Element item in elements)
+        {
+            if (item is T typedItem)
+            {
+                return typedItem;
+            }
+        }
+        return null;
+    }
+}
